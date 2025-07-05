0cb2edfe22cd1252ab563a139580b7d8d2ebbf8c
Kirill Mokevnin <mokevnin@gmail.com>
2025-07-04 19:02:36 -0500

    generate github page on commit
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./stylesheets/application.scss";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+	<App />,
 );
