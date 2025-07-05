94ec44de63dd595e83c009c4f057d089d85d848d
Kirill Mokevnin <mokevnin@gmail.com>
2025-07-05 17:18:52 -0500

    improve ui
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
+import { Carousel } from "@mantine/carousel";
 import {
-    ActionIcon,
+  ActionIcon,
   Avatar,
   Box,
   Button,
   Container,
   Group,
+  Image,
   Stack,
   Text,
   Title,
 } from "@mantine/core";
 import {
-    IconBrandGithub,
-  IconBrandInstagram,
+  IconBrandGithub,
+  IconBrandTelegram,
   IconBrandTwitter,
   IconBrandYoutube,
 } from "@tabler/icons-react";
-
 import MeImage from "./assets/me.png";
+import HexletImage from "./assets/hexlet.png";
+import CodeBasicsImage from "./assets/code-basics.png";
+import CodeBattleImage from "./assets/codebattle.png";
+
+const carouselData = [
+  {
+    image: HexletImage,
+    title: "hexlet.io",
+  },
+  {
+    image: CodeBasicsImage,
+    title: "code-basics.com",
+  },
+  {
+    image: CodeBattleImage,
+    // title: "code-basics.com",
+  },
+];
 
 function App() {
+  const slides = carouselData.map((item) => (
+    <Carousel.Slide key={item.title}>
+      <Image src={item.image} fit="cover" />
+      {/* {item.title} */}
+      {/* <CarouselCard {...item} /> */}
+    </Carousel.Slide>
+  ));
+
   return (
     <>
       <Container size="md" py={100}>
@@ -45,14 +72,6 @@ function App() {
 
         <Group>
           <Button
-            size="xl"
-            variant="gradient"
-            gradient={{ from: "blue", to: "cyan" }}
-          >
-            Projects
-          </Button>
-
-          <Button
             component="a"
             href="https://github.com/mokevnin"
             target="_blank"
@@ -62,21 +81,68 @@ function App() {
           >
             GitHub
           </Button>
+          <Button
+            size="xl"
+            component="a"
+            href="mailto:mokevnin@gmail.com"
+            variant="gradient"
+            gradient={{ from: "blue", to: "cyan" }}
+          >
+            Contact Me
+          </Button>
         </Group>
+      </Container>
+
+      <Container size="md" mb="xl">
+        <Carousel
+          withIndicators
+          // height={200}
+          emblaOptions={{
+            loop: true,
+            // dragFree: false,
+            align: "center",
+          }}
+          // slideSize={{ base: "100%", sm: "50%" }}
+          // slideGap={2}
+          // emblaOptions={{ align: "start" }}
+        >
+          {slides}
+        </Carousel>
       </Container>
 
       <Box>
         <Container>
           {/* <MantineLogo size={28} /> */}
           <Group gap={0} justify="flex-end" wrap="nowrap">
-            <ActionIcon size="lg" color="gray" variant="subtle">
+            <ActionIcon
+              component="a"
+              href="https://twitter.com/mokevnin"
+              target="_blank"
+              size="lg"
+              color="gray"
+              variant="subtle"
+            >
               <IconBrandTwitter size={18} stroke={1.5} />
             </ActionIcon>
-            <ActionIcon size="lg" color="gray" variant="subtle">
+            <ActionIcon
+              component="a"
+              href="https://www.youtube.com/@mokevnin"
+              target="_blank"
+              size="lg"
+              color="gray"
+              variant="subtle"
+            >
               <IconBrandYoutube size={18} stroke={1.5} />
             </ActionIcon>
-            <ActionIcon size="lg" color="gray" variant="subtle">
-              <IconBrandInstagram size={18} stroke={1.5} />
+            <ActionIcon
+              component="a"
+              href="https://t.me/orgprog"
+              target="_blank"
+              size="lg"
+              color="gray"
+              variant="subtle"
+            >
+              <IconBrandTelegram size={18} stroke={1.5} />
             </ActionIcon>
           </Group>
         </Container>
